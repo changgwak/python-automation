2b531ac1f50d1023ad76d33bf0f166a62e1e2008
changgwak <changgeun.usa@gmail.com>
2024-07-06 03:19:59 +0900

    Edit display order

--- pyautomation/displayinfo.py
+++ pyautomation/displayinfo.py
@@ -11,70 +11,73 @@ pyautomation is shared under the MIT Licene.
 This means that the code can be freely copied and distributed, and costs nothing to use.
 """
 
-
-# from .modules.PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication
 from .modules.screeninfo.screeninfo import get_monitors
 from .modules import mss
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication
-
 import sys
 
 class DisplayInfo():
     def __init__(self):
-   
         self.scale_factor = []
         self.display_info = []
-        self.is_app_generated =False
+        self.display_info_ = []
+        self.is_app_generated = False
         self.app = None
-        # self.get_scale_factor()
-        pass
 
     def get_Qapp(self):
-        if self.is_app_generated == False :
+        if self.is_app_generated == False:
             self.app = QApplication(sys.argv)
             self.is_app_generated = True
         return self.app
 
     def get_scale_factor(self, app):
         screens = app.screens()
-        for i, screen in enumerate(screens):
-            logical_dpi = screen.logicalDotsPerInch()
-            # print(screen, logical_dpi)
-            self.scale_factor.append(logical_dpi / 96.0)  # Based on Windows' standard DPI of 96
-        #     # print(f"Monitor {i+1}: Scale Factor = {self.scale_factors}")
+        monitor_order = get_monitors()  # Get the monitors in Windows display order
+        # print(f"monitor_order: {monitor_order}")
+        # screen_dict = {screen.geometry().topLeft(): screen for screen in screens}
+        # screen_dict = {tuple(screen.geometry().topLeft()): screen for screen in screens}
+        screen_dict = {(screen.geometry().topLeft().x(), screen.geometry().topLeft().y()): screen for screen in screens}
+        # print(f"screen_dict: {screen_dict}")
+
+
+        for monitor in monitor_order:
+            pos = (monitor.x, monitor.y)
+            # print(f"pos: {pos}")
+            screen = screen_dict.get(pos)
+            # print(f"screen: {screen}")
+            if screen:
+                logical_dpi = screen.logicalDotsPerInch()
+                self.scale_factor.append(logical_dpi / 96.0)  # Based on Windows' standard DPI of 96
         return self.scale_factor
-    
+
+    def get_screen_info_(self):
+        app = self.get_Qapp()
+        screens = app.screens()
+        for i, screen in enumerate(screens):
+            self.display_info_.append(f"Screen {i+1}: {screen.geometry()}")
+        return self.display_info_
 
     def get_screen_info(self):
         for m in get_monitors():
             self.display_info.append(str(m))
-            # print(str(m))
         return self.display_info
 
-    def print_scale_factors(self):
-        screens = self.app.screens()
-        for i, screen in enumerate(screens):
-            logical_dpi = screen.logicalDotsPerInch()
-            self.scale_factor = logical_dpi / 96.0  # Based on Windows' standard DPI of 96
-            print(f"Monitor {i+1}: Scale Factor = {self.scale_factor}")
-
-    def print_screen_info(self):
-        for m in get_monitors():
-            # self.display_info = self.display_info.append(str(m))
-            print(str(m))
-
-    def sreenshot(self):
-        # The simplest use, save a screen shot of the 1st monitor
-        with mss() as sct:
-            sct.shot()
+    def capture_screen(self) -> any:
+            with mss.mss() as sct:
+                monitor = sct.monitors
+            return monitor
 
 
 
-# if __name__ == "__main__":
-#     dis = DisplayInfo()
-#     # dis.print_scale_factors()
-#     # dis.print_screen_info()
-#     # print(dis.get_screen_info())
-#     print(dis.get_scale_factor(dis.get_Qapp()))
-    
+if __name__ == "__main__":
+    dis = DisplayInfo()
+    app = dis.get_Qapp()
+    print(dis.get_scale_factor(app))
+    print("##############")
+    print(dis.get_screen_info_())
+    print("##############")
+    print(dis.get_screen_info())
+    print("##############")
+    print(dis.capture_screen())
+
+
